04f477a736be47cab18a170b9da5a069b78975c2
cjvolpe <cjvolpe@unc.edu>
2026-02-20 01:07:29 -0500

    added basic game functionality wk2

--- projects/matching game/main.js
+++ projects/matching game/main.js
@@ -1,12 +1,61 @@
 const allTiles = document.querySelectorAll('.game-tile');
+
 console.log(allTiles);
-const colors = ["red", "green", "blue", "yellow", "orange", "pink","black","purple","red", "green", "blue", "yellow", "orange", "pink","black","purple"];
-// Randomize colors
-colors.sort(()=>0.5-Math.random());
-console.log(colors);
-for (let i = 1; i <= 50; i++) {
-    colors
+const colors = ["red", "green", "blue", "yellow", "orange", "pink", "black", "purple", "red", "green", "blue", "yellow", "orange", "pink", "black", "purple"];
+const restartButton = document.getElementById("restart");
+const moves = document.getElementById("moves");
+let moveCount;
+let lastTile = -1;
+let matchedTiles = [];
+let matched = 0;
+restartButton.addEventListener("click", start);
+
+for (let i = 0; i < allTiles.length; i++) {
+    allTiles[i].addEventListener("click", () => {
+        click(i);
+    });
 }
 
-//TODO: Add event listeners to button and for click
-//TODO: store last press
+// Randomize colors
+start();
+
+function start() {
+    colors.sort(() => 0.5 - Math.random());
+    moveCount = 0;
+    moves.textContent = "Moves: 0";
+
+    for (let x = 0; x < allTiles.length; x++) {
+        allTiles[x].style.backgroundColor = "gray";
+    }
+    console.log(colors);
+
+}
+
+function click(idx) {
+    if (matchedTiles.includes(allTiles[idx])) {
+        return;
+    }
+    allTiles[idx].style.backgroundColor = colors[idx];
+    moveCount++;
+    moves.textContent = "Moves: " + moveCount;
+    if (lastTile > -1) {
+        if (colors[idx] === colors[lastTile]) {
+            matchedTiles.push(allTiles[idx]);
+            matchedTiles.push(allTiles[lastTile]);
+            lastTile = -1;
+            matched++;
+        }
+        else{
+            allTiles[idx].style.backgroundColor = "gray";
+            allTiles[lastTile].style.backgroundColor = "gray";
+            lastTile = -1;
+        }
+    } else {
+        lastTile = idx;
+    }
+    if (matched === colors.length) {
+        alert("You Win! Click restart to play again!")
+    }
+}
+
+//TODO: add input buffering
